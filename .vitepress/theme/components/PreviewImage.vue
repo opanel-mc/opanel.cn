9c3cfe66bff6c60baeac8c54412b3fe85d9ba296
NriotHrreion <nriot233@outlook.com>
2026-04-22 09:10:00 +0800

    fix: preview image twinkles during theme switching

--- .vitepress/theme/components/PreviewImage.vue
+++ .vitepress/theme/components/PreviewImage.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { useData } from "vitepress";
+import { computed } from "vue";
 
 const { isDark } = useData();
 const { light, dark } = defineProps(["light", "dark"]);
+const srcUrl = computed(() => isDark.value ? dark : light);
 </script>
 
 <template>
   <ClientOnly>
     <div class="preview-container">
-      <img v-if="isDark" :src="dark" alt="preview-image"/>
-      <img v-else :src="light" alt="preview-image"/>
+      <img :src="srcUrl" alt="preview-image"/>
     </div>
   </ClientOnly>
 </template>
