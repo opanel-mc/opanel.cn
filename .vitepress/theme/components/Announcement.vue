ad3a1b785b239ed2a5ec2cce2afbb1323004e598
NriotHrreion <nriot233@outlook.com>
2026-04-26 18:07:31 +0800

    style: update announcement info and style

--- .vitepress/theme/components/Announcement.vue
+++ .vitepress/theme/components/Announcement.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="announcement-container">
     <div class="announcement">
-      <h2>OPanel 2.0 即将发布...</h2>
-      <span class="announcement-date">预计将于5月1日发布</span>
+      <h2>OPanel 2.0 现已发布 🎉</h2>
       <div class="announcement-features">
         <p>插件管理 背包编辑 MCP 服务器重启</p>
         <p>命令快捷方式 定时任务 全新UI界面</p>
@@ -36,30 +35,25 @@
   z-index: 10;
 }
 
-.announcement-container .announcement .announcement-date {
-  font-size: 10pt;
-  z-index: 10;
-}
-
 .announcement-container .announcement .announcement-features {
   font-size: 18pt;
   font-weight: bold;
   line-height: 45px;
   position: absolute;
-  right: 20%;
+  right: 5%;
 }
 
 .announcement-container .announcement .announcement-features p {
   width: fit-content;
   color: transparent;
-  background: linear-gradient(90deg, transparent 0%, var(--opanel-announcement-feat) 18%, var(--opanel-announcement-feat) 82%, transparent 100%);
+  background: linear-gradient(90deg, transparent 0%, var(--opanel-announcement-feat) 14%, var(--opanel-announcement-feat) 86%, transparent 100%);
   background-clip: text;
   -webkit-background-clip: text;
   white-space: nowrap;
   user-select: none;
 }
 
-.announcement-container .announcement .announcement-features p:nth-child(2) {
+.announcement-container .announcement .announcement-features p:nth-child(1) {
   margin-left: 70px;
 }
 
@@ -71,9 +65,12 @@
     padding: 15px 20px;
   }
 
+  .announcement-container .announcement h2 {
+    font-size: 10pt;
+  }
+
   .announcement-container .announcement .announcement-features {
-    top: 0;
-    right: 0;
+    display: none;
   }
 }
 
